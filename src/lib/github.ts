fe6591c79149bb0cfdeb7fd5f23abbb60ba7703a
widal001 <billy.daly@agile6.com>
2025-06-18 23:46:01 -0400

    feat: Adds fetchIssues() from GitHub

---
+++ src/lib/github.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+
+const IssueSchema = z.object({
+  url: z.string().url(),
+  title: z.string(),
+  body: z.string(),
+  labels: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      color: z.string(),
+    })
+  ),
+});
+
+export type Issue = z.infer<typeof IssueSchema>;
+
+type issueParams = {
+  owner: string;
+  repo: string;
+  labels: string[];
+  state?: "open" | "closed" | "all";
+  type?: string;
+};
+
+/**
+ * Fetch issues from a GitHub repository
+ * @param owner - The owner of the repository
+ * @param repo - The name of the repository
+ * @param state - The state of the issues
+ * @param labels - The labels of the issues
+ * @param type - The type of the issues
+ * @returns The parsed issues
+ */
+export async function fetchIssues({
+  owner,
+  repo,
+  state = "open",
+  labels,
+  type,
+}: issueParams): Promise<Issue[]> {
+  // Set query params
+  const query = new URLSearchParams();
+  query.set("state", state);
+  query.set("labels", labels.join(","));
+  query.set("per_page", "100");
+  query.set("page", "1");
+  if (type) {
+    query.set("type", type);
+  }
+
+  // Make request
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues?${query.toString()}`;
+  const response = await fetch(url);
+  const rawData = await response.json();
+
+  // Parse response directly with Zod array
+  return z.array(IssueSchema).parse(rawData);
+}
