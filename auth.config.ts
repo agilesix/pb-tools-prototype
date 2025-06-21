7e1c2df0ee4cfaaaca922d0fd55d6b86990794a1
Billy Daly <billy.daly@agile6.com>
2025-06-21 15:54:47 -0400

    [Issue #3] Fixes auth deploy (#10)
    * refactor: Changes how environment variables are loaded
    * fix: explicitly set secret to env var
    * feat: Adds script to set secrets
    * build: Adds preview config for PR test deploys
    * ci: Adds deploy preview to PR

--- auth.config.ts
+++ auth.config.ts
@@ -2,10 +2,11 @@ import Google from "@auth/core/providers/google";
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
+  secret: process.env.AUTH_SECRET,
   providers: [
     Google({
-      clientId: import.meta.env.GOOGLE_CLIENT_ID,
-      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
